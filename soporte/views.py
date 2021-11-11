fd97a40f055ad54ed12d15e3529394492d184678
David Torres <arq.dve@gmail.com>
2021-11-10 19:58:59 -0500

    Añadir vistas

--- soporte/views.py
+++ soporte/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import PersonaSoporteSerializers
+from .models import PersonaSoporte
+from .serializers import PQRSerializer
+from .models import PQR
 
-# Create your views here.
+
+class PersonaSoporteListCreate(generics.ListCreateAPIView):
+    #PersonaSoporte.objects.all se hacen los filtros heredados de la clase. En este caso se recibe todo por.all  ORM
+    queryset = PersonaSoporte.objects.all()
+    serializer_class = PersonaSoporteSerializers
+
+class PersonaSoporteUpdateDelete(generics.ListCreateAPIView):
+    queryset = PersonaSoporte.objects.all()
+    serializer_class = PersonaSoporteSerializers
+
+
+class PQRSerializerListCreate(generics.ListCreateAPIView):
+    #PersonaSoporte.objects.all se hacen los filtros heredados de la clase. En este caso se recibe todo por.all  ORM
+    queryset = PQR.objects.all()
+    serializer_class = PQRSerializer
+
+class PQRUpdateDelete(generics.ListCreateAPIView):
+    queryset = PQR.objects.all()
+    serializer_class = PQRSerializer
